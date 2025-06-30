79c9463af23affe837106032b8419a466da5e64d
eebreezy <selvin1982@yahoo.com>
2025-06-29 21:56:45 -0700

    Update game.js

--- game.js
+++ game.js
@@ -1,6 +1,15 @@
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+window.addEventListener("resize", () => {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+});
+
+
 // === AUDIO ===
 const bgMusic = new Audio("background-music.mp3");
 bgMusic.loop = true;
