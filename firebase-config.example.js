b1ce5dea8fba1b6bc7feb67d24f8168cb38a1b4c
eebreszy <selvin1982@yahoo.com>
2026-03-25 21:50:01 -0700

    new games

--- firebase-config.example.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "PASTE_YOUR_API_KEY",
-  authDomain: "PASTE_YOUR_PROJECT.firebaseapp.com",
-  projectId: "PASTE_YOUR_PROJECT_ID",
-  storageBucket: "PASTE_YOUR_PROJECT.appspot.com",
-  messagingSenderId: "PASTE_YOUR_SENDER_ID",
-  appId: "PASTE_YOUR_APP_ID"
-};
