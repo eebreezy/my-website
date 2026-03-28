ea8e647fea8073b0a73827cf69680a5594778d1b
eebreszy <selvin1982@yahoo.com>
2026-03-28 12:03:14 -0700

    deleted items

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
-  apiKey: "PASTE_YOUR_API_KEY",
-  authDomain: "PASTE_YOUR_PROJECT.firebaseapp.com",
-  projectId: "PASTE_YOUR_PROJECT_ID",
-  storageBucket: "PASTE_YOUR_PROJECT.appspot.com",
-  messagingSenderId: "PASTE_YOUR_SENDER_ID",
-  appId: "PASTE_YOUR_APP_ID"
-};
+    apiKey: "AIzaSyAkyHqCKoGKkG8WdsqZ8SzRQ9YtJEZh4bc",
+    authDomain: "user-uploads-omfunny.firebaseapp.com",
+    projectId: "user-uploads-omfunny",
+    storageBucket: "user-uploads-omfunny.firebasestorage.app",
+    messagingSenderId: "992631156301",
+    appId: "1:992631156301:web:e6bff4335a05c0b5ce3604",
+    measurementId: "G-7FRL224FYL"
+  };
